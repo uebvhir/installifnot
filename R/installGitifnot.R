13bebce3090d48cce87054338a92f0e52117b4b5
miriammota <mmota.foix@gmail.com>
2018-06-19 13:21:32 +0200

    require solucionat

--- R/installGitifnot.R
+++ R/installGitifnot.R
@@ -21,9 +21,10 @@ installGitifnot <- function(pathGit,
                             urlproxy = "conf_www.ir.vhebron.net", 
                             portproxy = 8081, 
                             force.install = FALSE){
-  if (!(require(pckgName, character.only = TRUE)) | force.install) {
+  if (!is.element(pckgName, installed.packages()[,1]) | force.install) {
     if (proxy) set_config(use_proxy(url = urlproxy, port = portproxy)) 
     install_github(file.path(pathGit,pckgName), force = force.install)
-    require(pckgName, character.only = TRUE)
   }
+    require(pckgName, character.only = TRUE)
+  
 }
